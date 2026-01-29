fc58679c320129671f24da89df536ca33da414a5
Mnumzana Moyo <mnumzanamoyo@Mnumzanas-Air.lan>
2026-01-29 00:54:53 -0500

    Fix import types

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Colors from '../constants/Colors';
-import { Product } from  '../types'
+import { Product } from  '@/types'
 
 export const defaultPizzaImage = 
     'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
